5982758b063f72f130b9853b6d733e45133291d4
Zzocker <pkspritam16@gmail.com>
2021-02-09 17:44:09 +0530

    adding bookStore port
    Signed-off-by: Zzocker <pkspritam16@gmail.com>

---
+++ model/book.go
@@ -0,0 +1,4 @@
+package model
+
+// Book :
+type Book struct{}
--- ports/ports.go
+++ ports/ports.go
@@ -33,14 +33,24 @@ type CommentStore interface {
 	Update(ctx context.Context, cmt model.Image) errors.E
 	Delete(ctx context.Context, cmtID string) errors.E
 	Query(ctx context.Context, sortKey string, query map[string]interface{}, pageNumber int64) ([]model.Comment, errors.E)
-	DeleteAll(ctx context.Context, username string) errors.E
+	DeleteAll(ctx context.Context, username string) errors.E // made by will be changed to bhuta
 }
 
-// TokenStore : port though which imageCore will interect with image database
+// TokenStore : port though which tokenCore will interect with token database
 type TokenStore interface {
 	Store(ctx context.Context, token model.Token) errors.E
 	Get(ctx context.Context, tokenID string) (*model.Token, errors.E)
 	Delete(ctx context.Context, tokeID string) errors.E
 	// DeleteAll : delete all tokens owned by a user
 	DeleteAll(ctx context.Context, userID string) errors.E
+}
+
+// BookStore : port though which bookCore will interect with book database
+type BookStore interface {
+	Store(ctx context.Context, book model.Book) errors.E
+	Get(ctx context.Context, bookID string) (*model.Book, errors.E)
+	Update(ctx context.Context, book model.Book) errors.E
+	Delete(ctx context.Context, bookID string) errors.E
+	Query(ctx context.Context, sortKey string, query map[string]interface{}, pageNumber int64) ([]model.Book, errors.E)
+	DeleteAll(ctx context.Context, username string) errors.E
 }
