75f8799d0cb1badfb49d1ed9ac55c939b2dd0238
Zzocker <pkspritam16@gmail.com>
2021-02-11 23:44:02 +0530

    adding blog
    Signed-off-by: Zzocker <pkspritam16@gmail.com>

--- pkg/blog/blog.go
+++ pkg/blog/blog.go
@@ -2,18 +2,48 @@
 
 package blog
 
+import (
+	"github.com/sirupsen/logrus"
+)
+
+// Level :
+type Level int8
+
+const (
+	// DebugLevel  : by - default debug level if off
+	DebugLevel Level = iota - 1
+
+	// InfoLevel default log level
+	InfoLevel
+)
+
 // Logger represents logger
 // it just interface
 // implantation TODO
 type Logger interface {
-	Infof(string, ...string)
-	Debugf(string, ...string)
-	Errorf(string, ...string)
-	WithFields(fields map[string]string) Logger
+	Infof(string, ...interface{})
+	Debugf(string, ...interface{})
+	Errorf(string, ...interface{})
 }
 
 // New Create blog logger
-func New() Logger {
-	// Implement me TODO
-	return nil
+func New(lvl Level) Logger {
+	l := logrus.New()
+	var level logrus.Level
+	if lvl == DebugLevel {
+		level = logrus.DebugLevel
+	} else {
+		level = logrus.InfoLevel
+	}
+	l.SetLevel(level)
+	return &logger{l}
+}
+
+// NewWithFields :
+func NewWithFields(l Logger, fields map[string]interface{}) Logger {
+	return l.(*logger).WithFields(logrus.Fields(fields))
+}
+
+type logger struct {
+	*logrus.Logger
 }
