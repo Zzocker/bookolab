417f7f617b76cbc11adf434f36493bf451adf493
Zzocker <pkspritam16@gmail.com>
2021-02-14 20:04:29 +0530

    adding insert doc with customID in mongo
    Signed-off-by: Zzocker <pkspritam16@gmail.com>

--- pkg/datastore/mongo_test.go
+++ pkg/datastore/mongo_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/Zzocker/bookolab/config"
 	"github.com/Zzocker/bookolab/pkg/blog"
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 var (
@@ -31,4 +33,26 @@ func TestNewMongoDS(t *testing.T) {
 	defer cancel()
 	ds = newMongoDS(ctx, blog.NewTestLogger(), conf)
 	assert.NotNil(t, ds)
+}
+
+type testStruct struct {
+	ID string `bson:"_id"`
+	F2 string `bson:"f2"`
+}
+
+func TestInsertWithCustomID(t *testing.T) {
+	id := primitive.NewObjectID().Hex()
+	v := testStruct{
+		ID: id,
+		F2: "name",
+	}
+	err := ds.Store(context.Background(), v)
+	assert.NoError(t, err)
+
+	raw, err := ds.Get(context.Background(), map[string]interface{}{
+		"_id": id,
+	})
+	var out testStruct
+	bson.Unmarshal(raw, &out)
+	assert.Equal(t, v, out)
 }
